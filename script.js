216dfae857700ab672f147e6b23117f97412b677
Sahba El-Shawa <sahba.elshawa@eurac.edu>
2025-07-30 15:17:53 +0200

    Create script.js for menu options

---
+++ script.js
@@ -0,0 +1,12 @@
+
+<!--  Script for toggling menu options  -->
+  <script>
+  document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const nav = document.querySelector(".nav-list");
+
+    burger.addEventListener("click", () => {
+      nav.classList.toggle("active");
+    });
+  });
+</script>
