1288e3ccfef4d06ba6acc31a8c6cdc9bfb99ed54
Sahba El-Shawa <sahba.elshawa@eurac.edu>
2025-07-31 10:35:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 // <!--  Script for toggling menu options  -->
   <script>
-  document.addEventListener("DOMContentLoaded", function () {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-list");
-
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("active");
+    document.addEventListener("DOMContentLoaded", function () {
+      const burger = document.querySelector(".burger");
+      const nav = document.querySelector(".nav-list");
+    
+      if (burger && nav) {
+        burger.addEventListener("click", function () {
+          nav.classList.toggle("active");
+        });
+      }
     });
-  });
 </script>
