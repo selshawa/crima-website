bf859fc391a1f005a194c58fbf61c9a32310a89e
Sahba El-Shawa <sahba.elshawa@eurac.edu>
2025-07-31 11:38:46 +0200

    Clean up script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+/**
+ * File: script.js
+ * Description:
+ * This script enables responsive navigation on mobile devices and small screens for the project website.
+ * It adds a click event listener to the menu toggle button to show or hide the navigation links.
+ * This functionality is only visible on screens smaller than 768px, as defined in the CSS.
+ *
+ * Author: Sahba El-Shawa
+ * Organization: Eurac Research
+ * Last updated: July 2025
+ */
 
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".burger");
